22a44371b73dadb79ea7e559c696d3b00197cc88
shivani-sahu1981 <shivanisahu1981@gmail.com>
2024-08-22 14:01:38 +0530

    main

---
+++ main.js
@@ -0,0 +1,77 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let cart = document.querySelector('.cart');
+
+document.querySelector('#cart-icon').onclick = () =>{
+    cart.classList.toggle('active');
+    search.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let user = document.querySelector('.user');
+
+document.querySelector('#user-icon').onclick = () =>{
+    user.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+}
+
+
+window.onscroll = () => {
+  navbar.classList.remove('active');
+  search.classList.remove('active');
+  cart.classList.remove('active');
+  user.classList.remove('active');
+}
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll',() => {
+  header.classList.toggle('shadow',window.scrollY > 0);
+});
+
+
+
+var swiper = new Swiper(".new-arrival", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      sidesPerView: 0,
+    },
+    568: {
+      sidesPerView: 2,
+    },
+    768: {
+      sidesPerView:2 ,
+    },
+    1020: {
+      sidesPerView: 3,
+    }
+  },
+});
+
+
